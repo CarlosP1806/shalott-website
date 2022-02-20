8160c0ad5a7aef3a01f3e99d3707b7cbc6692889
Carlos Paez <carlosalbertopaez2002@gmail.com>
2022-02-20 12:25:37 -0600

    Create fake db for development

--- controllers/home/home_routes.js
+++ controllers/home/home_routes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
+const db = require('../../db/products.json');
 
 router.get('/', (req, res) => {
-    res.render('homepage');
+    res.render('homepage', { products: db });
 })
 
 module.exports = router;
