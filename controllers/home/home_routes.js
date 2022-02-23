910e48bf0f0b6bf52c596d5c2271992ca7e46f2e
Carlos Paez <carlosalbertopaez2002@gmail.com>
2022-02-22 19:33:40 -0600

    Create and style categories section

--- controllers/home/home_routes.js
+++ controllers/home/home_routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-const db = require('../../db/products.json');
+const db_products = require('../../db/products.json');
+const db_categories = require('../../db/categories.json');
 
 router.get('/', (req, res) => {
-    res.render('homepage', { products: db });
+    res.render('homepage', { products: db_products, categories: db_categories });
 })
 
 module.exports = router;
