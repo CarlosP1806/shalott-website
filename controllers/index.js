efb49716dcc33f2c10f1aa869480aa4b6388ad00
Carlos Paez <carlosalbertopaez2002@gmail.com>
2022-03-02 20:40:48 -0600

    Refactor catalogue to use db instead of static file

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const homeRoutes = require('./home/home_routes');
+const catalogeRoutes = require('./catalogue/catalogue_routes');
 
 router.use('/', homeRoutes);
+router.use('/catalogue', catalogeRoutes);
 
 module.exports = router;
