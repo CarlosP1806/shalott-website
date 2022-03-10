419843817a34f8609d9a23440f5d72ae8c600731
Carlos Paez <carlosalbertopaez2002@gmail.com>
2022-03-09 19:51:25 -0600

    Fix banner btn link

--- public/js/banner.js
+++ public/js/banner.js
@@ -1,3 +1,8 @@
+const bannerBtn = document.querySelector('.banner__btn');
+bannerBtn.addEventListener("click", () => {
+  document.location.replace('/catalogue');
+})
+
 let counter = 1;
 document.getElementById('radio' + counter).checked = true;
 setInterval(function () {
