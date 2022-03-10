3349d5266158898e0db5c7e99d69197cee60c16c
Carlos Paez <carlosalbertopaez2002@gmail.com>
2022-03-09 18:57:51 -0600

    Fix banner image transition timing

--- public/js/banner.js
+++ public/js/banner.js
@@ -1,6 +1,7 @@
 let counter = 1;
+document.getElementById('radio' + counter).checked = true;
 setInterval(function () {
-  document.getElementById('radio' + counter).checked = true;
   counter++;
   if(counter > 4) counter = 1;
+  document.getElementById('radio' + counter).checked = true;
 }, 5000);
