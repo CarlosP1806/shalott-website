560dc3d8bdedc3394ecbdde86bef353fc0acfab7
Carlos Paez <carlosalbertopaez2002@gmail.com>
2022-03-09 19:58:35 -0600

    Add aretes and collares views

--- controllers/catalogue/catalogue_routes.js
+++ controllers/catalogue/catalogue_routes.js
@@ -3,7 +3,17 @@ const Product = require('../../models/Product');
 
 router.get('/', async (req, res) => {
   const products = await Product.find({});
-  res.render('catalogue', { products: products });
+  res.render('catalogue', { products: products, title: "Todos los productos" });
+});
+
+router.get('/aretes', async (req, res) => {
+  const products = await Product.find({ productCategory: "Aretes" });
+  res.render('catalogue', { products: products, title: "Aretes" });
+});
+
+router.get('/collares', async (req, res) => {
+  const products = await Product.find({ productCategory: "Collages" });
+  res.render('catalogue', { products: products, title: "Collares" });
 });
 
 
