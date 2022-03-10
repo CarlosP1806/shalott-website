f2c6a323a4eb057a889e2ed721968cdcc9fc11e4
Carlos Paez <carlosalbertopaez2002@gmail.com>
2022-03-09 20:38:21 -0600

    Implement category routing

--- controllers/catalogue/catalogue_routes.js
+++ controllers/catalogue/catalogue_routes.js
@@ -1,19 +1,25 @@
 const router = require('express').Router();
 const Product = require('../../models/Product');
+const Category = require('../../models/Category');
 
 router.get('/', async (req, res) => {
   const products = await Product.find({});
-  res.render('catalogue', { products: products, title: "Todos los productos" });
+  res.render('catalogue', { products: products, title: "Todos los productos", categories: false });
+});
+
+router.get('/categorias', async (req, res) => {
+  const categories = await Category.find({});
+  res.render('catalogue', { products: categories, title: "Categorias", categories: true});
 });
 
 router.get('/aretes', async (req, res) => {
   const products = await Product.find({ productCategory: "Aretes" });
-  res.render('catalogue', { products: products, title: "Aretes" });
+  res.render('catalogue', { products: products, title: "Aretes", categories: false });
 });
 
 router.get('/collares', async (req, res) => {
   const products = await Product.find({ productCategory: "Collages" });
-  res.render('catalogue', { products: products, title: "Collares" });
+  res.render('catalogue', { products: products, title: "Collares", categories: false });
 });
 
 
