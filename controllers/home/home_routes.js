5df893f8413ec2d9bf4aab4966ce4993506e7ccf
Carlos Paez <carlosalbertopaez2002@gmail.com>
2022-03-14 12:38:39 -0600

    Create Collection model

--- controllers/home/home_routes.js
+++ controllers/home/home_routes.js
@@ -2,13 +2,17 @@ const router = require('express').Router();
 
 const Product = require('../../models/Product.js');
 const Category = require('../../models/Category.js');
+const Collection = require('../../models/Collection.js');
 
 router.get('/', async (req, res) => {
   const featuredProducts = await Product.find({ featured: true });
   const categories = await Category.find({});
+  const collections = await Collection.find({});
+
   res.render('homepage', {
     products: featuredProducts, 
-    categories: categories
+    categories: categories,
+    collections: collections
   });
 });
 
