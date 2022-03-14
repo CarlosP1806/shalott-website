4cf1bb5963e16d926a9412566acb387092bb1e31
Carlos Paez <carlosalbertopaez2002@gmail.com>
2022-03-14 13:17:20 -0600

    Update routes to accept both category and collection filtering

--- controllers/catalogue/catalogue_routes.js
+++ controllers/catalogue/catalogue_routes.js
@@ -6,31 +6,31 @@ const Collection = require('../../models/Collection');
 // GET all products
 router.get('/', async (req, res) => {
   const products = await Product.find({});
-  res.render('catalogue', { products: products, title: "Todos los productos", categories: false });
+  res.render('catalogue', { products: products, title: "Todos los productos", groups: false, collections: false });
 });
 
 // GET all categories
 router.get('/categorias', async (req, res) => {
   const categories = await Category.find({});
-  res.render('catalogue', { products: categories, title: "Categorias", categories: true});
+  res.render('catalogue', { products: categories, title: "Categorias", groups: true, collections : false});
 });
 
 // GET products from given category
 router.get('/categorias/:categoria', async (req, res) => {
   const products = await Product.find({ productCategory: req.params.categoria });
-  res.render('catalogue', { products: products, title: req.params.categoria, categories: false});
+  res.render('catalogue', { products: products, title: req.params.categoria, groups: false, collections: false});
 });
 
 // GET all collections
 router.get('/colecciones', async (req, res) => {
   const collections = await Collection.find({});
-  res.render('catalogue', { products: collections, title: "Colecciones", categories: true });
+  res.render('catalogue', { products: collections, title: "Colecciones", groups: true, collections: true });
 });
 
 // GET products from given collection
 router.get('/colecciones/:coleccion', async (req, res) => {
   const products = await Product.find({ productCollection: req.params.coleccion });
-  res.render('catalogue', { products: products, title: req.params.coleccion, categories: false});
+  res.render('catalogue', { products: products, title: req.params.coleccion, groups: false, collections: false});
 });
 
 module.exports = router;
