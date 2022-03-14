1c173797987c71974bb74f6bd7ef3cd01a8c1d30
Carlos Paez <carlosalbertopaez2002@gmail.com>
2022-03-14 12:53:32 -0600

    Update categories and collections routing

--- controllers/catalogue/catalogue_routes.js
+++ controllers/catalogue/catalogue_routes.js
@@ -9,15 +9,28 @@ router.get('/', async (req, res) => {
   res.render('catalogue', { products: products, title: "Todos los productos", categories: false });
 });
 
-// GET categories
+// GET all categories
 router.get('/categorias', async (req, res) => {
   const categories = await Category.find({});
   res.render('catalogue', { products: categories, title: "Categorias", categories: true});
 });
 
+// GET products from given category
 router.get('/categorias/:categoria', async (req, res) => {
   const products = await Product.find({ productCategory: req.params.categoria });
   res.render('catalogue', { products: products, title: req.params.categoria, categories: false});
+});
+
+// GET all collections
+router.get('/colecciones', async (req, res) => {
+  const collections = await Collection.find({});
+  res.render('catalogue', { products: collections, title: "Colecciones", categories: true });
+});
+
+// GET products from given collection
+router.get('/colecciones/:coleccion', async (req, res) => {
+  const products = await Collection.find({ productCollection: req.params.coleccion });
+  res.render('catalogue', { products: products, title: req.params.coleccion, categories: false});
 });
 
 module.exports = router;
