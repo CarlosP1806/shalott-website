f28b60b177659bfc268a7784f6dd5b42abaf67d5
Carlos Paez <carlosalbertopaez2002@gmail.com>
2022-03-17 15:33:15 -0600

    Install dotenv for securing info

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const sessions = require('express-session');
+require('dotenv').config();
+
 const routes = require('./controllers');
 const db = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Setup admin session
+const oneDay = 1000 * 60 * 60 * 24;
+app.use(sessions({
+    secret:
+}))
 
 app.use(express.static('public'));
 app.use(express.json());
