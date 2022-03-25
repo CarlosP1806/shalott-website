9e59779d146849655dbc3edffd8e97cba590b1db
Carlos Paez <carlosalbertopaez2002@gmail.com>
2022-03-24 19:44:32 -0600

    Update product collection on delete

--- controllers/admin/admin_routes.js
+++ controllers/admin/admin_routes.js
@@ -95,6 +95,7 @@ router.delete('/delete/collection/', async (req, res) => {
       res.status(404).json("No collection found");
       return;
     }
+    await Product.updateMany({ productCollection: req.body.name }, { productCollection: ""});
     res.status(200).json(collection);
   } catch(err) {
     res.status(500).json(err);
