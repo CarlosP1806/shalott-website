509e0c006b8c475d3d1596cbfbba5f754b28ddf9
Carlos Paez <carlosalbertopaez2002@gmail.com>
2022-04-12 18:28:52 -0500

    Send confirmatio email

--- controllers/home/home_routes.js
+++ controllers/home/home_routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
+const nodemailer = require('nodemailer');
 
 const Product = require('../../models/Product.js');
 const Category = require('../../models/Category.js');
@@ -21,8 +22,56 @@ router.get('/success', async (req, res) => {
   try {
     const session = await stripe.checkout.sessions.retrieve(req.query.session_id);
     const customer = await stripe.customers.retrieve(session.customer);
-    console.log(customer);
+    const lineItems = await stripe.checkout.sessions.listLineItems(req.query.session_id);
+
+    // Send verification email
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.DB_SERVER_EMAIL,
+        pass: process.env.DB_SERVER_PASSWORD
+      }
+    });
+
+    let message = "";
+    message += `Cliente ${customer.name} ha solicitado lo siguiente:`;
+    message += '<ul>'
+    // Get buyed items
+    lineItems.data.forEach(item => {
+      message += "<li>";
+      message += "Artículo: "
+      message += item.description;
+      message += " - Cantidad: ";
+      message += item.quantity;
+      message += "</li>";
+    });
+    message += '</ul>'
+    message += `<p>La información de contacto del comprador es:</p>`;
+    message += `<p>Email: ${customer.email} </p>`;
+    message += `<p>Teléfono: ${customer.phone} </p>`;
+    message += `<p>
+      Para cualquier aclaración del pediddo, 
+      favor de contactar al número 442-1234-5678 presentando el id
+      de compra </p><p>${session.payment_intent}
+      </p>`
+
+    let mailOptions = {
+      from: process.env.DB_SERVER_EMAIL,
+      to: process.env.DB_SERVER_EMAIL,
+      subject: "Nuevo pedido de producto",
+      html: message
+    };
+
+    transporter.sendMail(mailOptions, function (err, data) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Email sent");
+      }
+    });
+
   } catch (e) {
+    console.log(e);
     res.render('message', {
       message: "Lo sentimos, la página que usted busca no se encuentra en el servidor"
     });
