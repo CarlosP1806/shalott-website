fc674559fff1894f6445d6a4b6f1f419a4b029c6
Carlos Paez <carlosalbertopaez2002@gmail.com>
2022-04-13 13:22:29 -0500

    Fix collection glider in big screens

--- public/js/glider.js
+++ public/js/glider.js
@@ -1,4 +1,4 @@
-const numberCollections = 
+const numberCollections =
   parseInt(document.querySelector('.section--collections').dataset.numberCollections);
 
 window.addEventListener("load", () => {
@@ -28,3 +28,12 @@ window.addEventListener("load", () => {
     ]
   });
 });
+
+if (window.matchMedia('(min-width: 1200px)').matches) {
+  if (numberCollections <= 4) {
+    const carouselNext = document.querySelector('.carousel__next');
+    carouselNext.style.display = "none";
+    const carouselPrev = document.querySelector('.carousel__prev');
+    carouselPrev.style.display = "none";
+  }
+}
