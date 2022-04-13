d16d1b41062d8a02dad1e8d0fb4d66b27e1ca29b
Carlos Paez <carlosalbertopaez2002@gmail.com>
2022-04-13 14:18:08 -0500

    Update glider in big screens

--- public/js/glider.js
+++ public/js/glider.js
@@ -27,4 +27,13 @@ window.addEventListener("load", () => {
       }
     ]
   });
+  
+  if (window.matchMedia('(min-width: 1200px)').matches) {
+    if (numberCollections <= 4) {
+      const carouselNext = document.querySelector('.carousel__next');
+      carouselNext.style.display = "none";
+      const carouselPrev = document.querySelector('.carousel__prev');
+      carouselPrev.style.display = "none";
+    }
+  }
 });
