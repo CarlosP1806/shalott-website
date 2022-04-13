a3e7f57d88734144e9e26d402e09df6633263c15
Carlos Paez <carlosalbertopaez2002@gmail.com>
2022-04-13 14:14:27 -0500

    Fix glider error in iphone

--- public/js/glider.js
+++ public/js/glider.js
@@ -2,15 +2,6 @@ const numberCollections =
   parseInt(document.querySelector('.section--collections').dataset.numberCollections);
 
 window.addEventListener("load", () => {
-  if (window.matchMedia('(min-width: 1200px)').matches) {
-    if (numberCollections <= 4) {
-      const carouselNext = document.querySelector('.carousel__next');
-      carouselNext.style.display = "none";
-      const carouselPrev = document.querySelector('.carousel__prev');
-      carouselPrev.style.display = "none";
-    }
-  }
-  
   new Glider(document.querySelector('.carousel__list'), {
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -36,4 +27,14 @@ window.addEventListener("load", () => {
       }
     ]
   });
+  
+  if (window.matchMedia('(min-width: 1200px)').matches) {
+    if (numberCollections <= 4) {
+      const carouselNext = document.querySelector('.carousel__next');
+      carouselNext.style.display = "none";
+      const carouselPrev = document.querySelector('.carousel__prev');
+      carouselPrev.style.display = "none";
+    }
+  }
+
 });
