6fcc8fc57d4ef042fa7ddbd4550eb5b1547044ea
Carlos Paez <carlosalbertopaez2002@gmail.com>
2022-04-13 16:42:47 -0500

    Restore old glider

--- public/js/glider.js
+++ public/js/glider.js
@@ -2,38 +2,38 @@ const numberCollections =
   parseInt(document.querySelector('.section--collections').dataset.numberCollections);
 
 window.addEventListener("load", () => {
-  $('.carousel__list').slick({
-    dots: true,
-    infinite: true,
-    prevArrow: '.carousel__prev',
-    nextArrow: '.carousel__next',
-    appendDots: '.carousel__indicators',
-    slidesToShow: 4,
-    slidesToScroll: 4,
+  new Glider(document.querySelector('.carousel__list'), {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: '.carousel__indicators',
+    arrows: {
+      prev: '.carousel__prev',
+      next: '.carousel__next'
+    },
     responsive: [
       {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
+        breakpoint: 768,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2
         }
       },
       {
-        breakpoint: 480,
+        breakpoint: 1200,
         settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToShow: numberCollections >= 4 ? 4 : numberCollections,
+          slidesToScroll: numberCollections >= 4 ? 4 : numberCollections
         }
       }
     ]
   });
 });
+
+if (window.matchMedia('(min-width: 1200px)').matches) {
+  if (numberCollections <= 4) {
+    const carouselNext = document.querySelector('.carousel__next');
+    carouselNext.style.display = "none";
+    const carouselPrev = document.querySelector('.carousel__prev');
+    carouselPrev.style.display = "none";
+  }
+}
