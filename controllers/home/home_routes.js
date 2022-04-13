4b4b9f7f150c303ac0765a7779d65db69b959426
Carlos Paez <carlosalbertopaez2002@gmail.com>
2022-04-12 20:26:05 -0500

    Style email format

--- controllers/home/home_routes.js
+++ controllers/home/home_routes.js
@@ -34,24 +34,47 @@ router.get('/success', async (req, res) => {
     });
 
     let message = "";
-    message += `Cliente ${customer.name} ha solicitado lo siguiente:`;
-    message += '<ul>'
+    message += `<h1 style="color: green; text-align:center;">¡Pago Exitoso!</h1>`
+    message += `<p>Estimado ${customer.name},</p>`;
+    message += `<p>
+    Tu compra ha sido procesada y realizada con éxito. Nos encontramos trabajando
+    en tu pedido. En cuanto el envío sea realizado, nos comunicaremos contigo para
+    indicarte el número de seguimiento.
+    </p>`;
+    message += '<hr style="background: gray;">'
+    message += '<h2>Resumen de Orden</h2>'
+    message += '<table>'
+    message += `
+    <tr>
+      <th>Producto</th>
+      <th>Cantidad</th>
+      <th>Subtotal</th>
+    </tr>
+    `
     // Get buyed items
     lineItems.data.forEach(item => {
-      message += "<li>";
-      message += "Artículo: "
-      message += item.description;
-      message += " - Cantidad: ";
-      message += item.quantity;
-      message += "</li>";
+      message += "<tr>";
+      message += `<td>${item.description}</td>`;
+      message += `<td style="text-align: right;">${item.quantity}</td>`;
+      message += `<td style="text-align: right;">${(item.amount_total / 100).toFixed(2)}</td>`;
+      message += "</tr>";
     });
-    message += '</ul>'
-    message += `<p>La información de contacto del comprador es:</p>`;
+    message += '</table>'
+    message += `<p style="font-weight: bold;">Total de compra: ${(session.amount_total / 100).toFixed(2)}</p>`
+    message += '<hr style="background: gray;">'
+    message += '<h2>Detalles de Envío</h2>'
     message += `<p>Email: ${customer.email} </p>`;
     message += `<p>Teléfono: ${customer.phone} </p>`;
+    message += `<p>Dirección de envío:</p>`
+    message += `<p style="margin-left: 2rem;">${customer.shipping.address.city}, ${customer.shipping.address.country}</p>`;
+    message += `<p style="margin-left: 2rem;">${customer.shipping.address.line1}</p>`;
+    message += `<p style="margin-left: 2rem;">${customer.shipping.address.line2}</p>`;
+    message += `<p style="margin-left: 2rem;">${customer.shipping.address.postal_code}</p>`;
+    message += `<p style="margin-left: 2rem;">${customer.shipping.address.state}</p>`;
+
     message += `<p>
-      Para cualquier aclaración del pediddo, 
-      favor de contactar al número 442-1234-5678 presentando el id
+      Para cualquier duda o aclaración del pedido, 
+      favor de contactar al número 442-1234-5678 presentando el siguiente identificador
       de compra </p><p>${session.payment_intent}
       </p>`
 
