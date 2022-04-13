1a2a5d132ab5ee4e99aef24ee11a4cd9794c6619
Carlos Paez <carlosalbertopaez2002@gmail.com>
2022-04-12 20:43:30 -0500

    Update email message

--- controllers/home/home_routes.js
+++ controllers/home/home_routes.js
@@ -105,11 +105,11 @@ function createEmailMessage(customer, lineItems, session) {
   message += `<p>Email: ${customer.email} </p>`;
   message += `<p>Teléfono: ${customer.phone} </p>`;
   message += `<p>Dirección de envío:</p>`
-  message += `<p style="margin-left: 2rem;">${customer.shipping.address.city}, ${customer.shipping.address.country}</p>`;
-  message += `<p style="margin-left: 2rem;">${customer.shipping.address.line1}</p>`;
-  message += `<p style="margin-left: 2rem;">${customer.shipping.address.line2}</p>`;
-  message += `<p style="margin-left: 2rem;">${customer.shipping.address.postal_code}</p>`;
-  message += `<p style="margin-left: 2rem;">${customer.shipping.address.state}</p>`;
+  message += `<p style="margin-left: 2rem;">Ciudad: ${customer.shipping.address.city}, ${customer.shipping.address.country}</p>`;
+  message += `<p style="margin-left: 2rem;">Línea 1: ${customer.shipping.address.line1}</p>`;
+  message += `<p style="margin-left: 2rem;">Línea 2: ${customer.shipping.address.line2}</p>`;
+  message += `<p style="margin-left: 2rem;">CP: ${customer.shipping.address.postal_code}</p>`;
+  message += `<p style="margin-left: 2rem;">Estado: ${customer.shipping.address.state}</p>`;
   message += `<p>
       Para cualquier duda o aclaración del pedido, 
       favor de contactar al número 442-1234-5678 presentando el siguiente identificador
