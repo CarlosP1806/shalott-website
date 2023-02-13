0defd289cc061c08e1d5f782ed175a2ca3715576
Carlos Paez <carlosalbertopaez2002@gmail.com>
2023-02-13 12:03:52 -0600

    Refactor add to cart logic

--- public/js/add_to_cart.js
+++ public/js/add_to_cart.js
@@ -1,43 +1,49 @@
 const LOCAL_STORAGE_CART_KEY = 'shalott.cart';
+const MAX_CART_ITEMS = 5;
+
+// Load cart items from local storage or create empty array
 const cartItems = JSON.parse(localStorage.getItem(LOCAL_STORAGE_CART_KEY)) || [];
 
 function saveCart() {
   localStorage.setItem(LOCAL_STORAGE_CART_KEY, JSON.stringify(cartItems));
 }
 
-const addToCartForm = document.querySelector('.display__form');
-const addToCartBtn = document.querySelector('.display__btn');
-addToCartForm.addEventListener('submit', event => {
+function updateCart(product) {
+  const {
+    productId, 
+    productQuantity, 
+  } = product;
+
+  const duplicatedIndex = cartItems.findIndex(item => item.productId == productId);
+  if(duplicatedIndex !== -1) {
+    updateProductQuantity(duplicatedIndex, parseInt(productQuantity));
+  } else {
+    addNewItem(product);
+  }
+}
+
+function updateProductQuantity(index, quantity) {
+  const newQuantity = Math.min(parseInt(cartItems[index].productQuantity)+parseInt(quantity), MAX_CART_ITEMS);
+  cartItems[index].productQuantity = newQuantity;
+  saveCart();
+}
+
+function addNewItem(item) {
+  cartItems.push(item);
+  saveCart();
+}
+
+function handleAddToCart(event) {
   event.preventDefault();
 
   const productId = addToCartBtn.dataset.productId;
-  const itemQuantity = document.querySelector('#product-amount').value;
+  const productName = addToCartBtn.dataset.productName;
+  const productImage = addToCartBtn.dataset.productImage;
+  const productPrice = addToCartBtn.dataset.productPrice;
+  const productQuantity = document.querySelector('#product-amount').value;
+  const productSlug = addToCartBtn.dataset.productSlug;
 
-  let duplicate = false;
-  cartItems.forEach(item => {
-    if (item.productId === productId) duplicate = true;
-  })
-
-  if (!duplicate) {
-    const newItem = {
-      productId: addToCartBtn.dataset.productId,
-      productName: addToCartBtn.dataset.productName,
-      productImage: addToCartBtn.dataset.productImage,
-      productPrice: addToCartBtn.dataset.productPrice,
-      productQuantity: itemQuantity,
-      productSlug: addToCartBtn.dataset.productSlug
-    }
-    cartItems.push(newItem);
-  } else {
-    const cartItemIndex = cartItems.findIndex(item => item.productId === productId);
-    cartItems[cartItemIndex].productQuantity =
-      parseInt(cartItems[cartItemIndex].productQuantity) + parseInt(itemQuantity);
-
-    if (cartItems[cartItemIndex].productQuantity > 5)
-      cartItems[cartItemIndex].productQuantity = 5;
-  }
-
-  saveCart();
+  updateCart({productId, productName, productImage, productPrice, productQuantity, productSlug});
 
   const successModal = document.querySelector('.add-cart-modal');
   const modalOverlay = document.querySelector('.modal__overlay');
@@ -48,4 +54,9 @@ addToCartForm.addEventListener('submit', event => {
     successModal.classList.remove('active');
     modalOverlay.classList.remove('active');
   })
-});
+}
+
+const addToCartForm = document.querySelector('.display__form');
+const addToCartBtn = document.querySelector('.display__btn');
+
+addToCartForm.addEventListener('submit', handleAddToCart);
