07f6e84aa0cbbac83f18e88b7d9b8a3a5e3de9c6
Carlos Paez <carlosalbertopaez2002@gmail.com>
2023-02-13 11:27:04 -0600

    Refactor cart routes

---
+++ controllers/store_controlers.js
@@ -0,0 +1,52 @@
+const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
+const Product = require('../models/Product');
+require('dotenv').config();
+
+async function renderShoppingCart(req, res) {
+  res.render('shopping_cart');
+}
+
+async function createCheckoutSession(req, res) {
+  try {
+    // Find all items with id in request
+    const ids = req.body.items.map(item => {
+      return item.id;
+    });
+    const products = await Product.find({ '_id': { $in: ids } });
+    const lineItems = products.map(item => {
+      const quantity = req.body.items.find(reqItem => reqItem.id === item.id).quantity;
+      return {
+        price_data: {
+          currency: 'mxn',
+          product_data: {
+            name: item.productName
+          },
+          unit_amount: item.productPrice * 100
+        },
+        quantity: quantity
+      }
+    });
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      shipping_address_collection: {
+        allowed_countries: ['MX'],
+      },
+      phone_number_collection: {
+        enabled: true,
+      },
+      mode: 'payment',
+      line_items: lineItems,
+      success_url: `${process.env.SERVER_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${process.env.SERVER_URL}/cancel`
+    });
+    res.json({ url: session.url });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
+
+module.exports = {
+    renderShoppingCart,
+    createCheckoutSession
+}
--- routes/store/shopping_cart_routes.js
+++ routes/store/shopping_cart_routes.js
@@ -1,51 +1,12 @@
 const router = require('express').Router();
-const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
-const Product = require('../../models/Product');
 require('dotenv').config();
 
-router.get('/', (req, res) => {
-  res.render('shopping_cart');
-});
+const {
+  renderShoppingCart,
+  createCheckoutSession
+} = require('../../controllers/store_controlers');
 
-router.post('/create-checkout-session', async (req, res) => {
-  try {
-    // Find all items with id in request
-    const ids = req.body.items.map(item => {
-      return item.id;
-    });
-    const products = await Product.find({ '_id': { $in: ids } });
-    const lineItems = products.map(item => {
-      const quantity = req.body.items.find(reqItem => reqItem.id === item.id).quantity;
-      return {
-        price_data: {
-          currency: 'mxn',
-          product_data: {
-            name: item.productName
-          },
-          unit_amount: item.productPrice * 100
-        },
-        quantity: quantity
-      }
-    });
-
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      shipping_address_collection: {
-        allowed_countries: ['MX'],
-      },
-      phone_number_collection: {
-        enabled: true,
-      },
-      mode: 'payment',
-      line_items: lineItems,
-      success_url: `${process.env.SERVER_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `${process.env.SERVER_URL}/cancel`
-    });
-    res.json({ url: session.url });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
+router.route('/').get(renderShoppingCart);
+router.route('/create-checkout-session').post(createCheckoutSession);
 
 module.exports = router;
